75c542b47773c465c68d2a9b5a5ff45d774f1f7a
Aleksey Pivovarov <AMPivovarov@gmail.com>
2021-01-31 03:38:14 +0300

    update build configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  kotlin("jvm") version "1.3.31"
+  kotlin("jvm") version "1.3.72"
   id("org.jetbrains.intellij") version "0.4.8"
 }
 
@@ -12,11 +12,12 @@ version = "1.5.7"
 
 // See https://github.com/JetBrains/gradle-intellij-plugin/
 intellij {
-  version = "IC-2019.3"
+  version = "IC-2021.1"
 }
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
   kotlinOptions.jvmTarget = "1.8"
+  kotlinOptions.freeCompilerArgs = listOf("-Xjvm-default=enable")
 }
 
 tasks {
