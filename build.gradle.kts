5df382b5f67df8869b4b8ce171d84709215107d4
Aleksey Pivovarov <AMPivovarov@gmail.com>
2023-04-13 15:08:30 +0200

    v1.6.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ repositories {
 }
 
 group = "com.jetbrains"
-version = "1.6.0"
+version = "1.6.1"
 
 // https://github.com/JetBrains/gradle-intellij-plugin/
 // https://www.jetbrains.com/intellij-repository/snapshots/
